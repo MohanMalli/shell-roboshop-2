1eb1c92d964f386e9f0deba1ebf7fc3f4ab72fd4
MohanMalli <mmohan0140@gmail.com>
2025-05-29 17:08:25 +0530

    Roboshop Project

---
+++ catalogue.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+souce ./common.sh
+app_name=catalogue
+
+check_root
+app_setup
+nodejs_setup
+systemd_setup
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>LOGS_FILE
+VALIDATE $? "Installing Mongodb clinet"
+
+STATUS=$(mongosh --host mongodb.malli.site --eval 'db.getMongo().gitDBNames().indexof( "catalogue")')
+if[ $STATUS -lt 0 ]
+then 
+    mongosh --host mongodb.malli.site </app/db/master-date.js &>>LOG_FILE
+    VALIDATE $? "Loading data into MongoDB"
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi
+
+print_time
+
