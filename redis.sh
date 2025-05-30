56b28c69f325a1b7de74194271151807fd4a3193
MohanMalli <mmohan0140@gmail.com>
2025-05-30 21:45:21 +0530

    Roboshop Project

--- redis.sh
+++ redis.sh
@@ -9,13 +9,13 @@ dnf module disable redis -y &>>LOG_FILE
 VALIDATE $? "Disabling Default Redis Version"
 
 dnf module enable redis:7 -y &>>LOG_FILE
-VALIDATE $? "Enabling redis:7"
+VALIDATE $? "Enabling Redis:7"
 
 dnf install redis -y &>>LOG_FILE
 VALIDATE $? "Installing Redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no ' /etc/redis/redis.conf
-VALIDATE $? "Edited redis.conf to accept remote connections"
+VALIDATE $? "Edited Redis.Conf to accept remote connections"
 
 
 systemctl enable redis &>>$LOG_FILE
